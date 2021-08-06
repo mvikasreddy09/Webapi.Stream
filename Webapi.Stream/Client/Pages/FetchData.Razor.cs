67aa38986bd076003fd3ad9b629baa32e1f0de9a
Vikas Reddy <Vikas@mtccrm.com>
2021-08-06 18:09:26 +0530

    Add project files.

---
+++ Webapi.Stream/Client/Pages/FetchData.Razor.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Components.WebAssembly.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Webapi.Stream.Shared;
+
+namespace Webapi.Stream.Client.Pages
+{
+    public partial class FetchData
+    {
+        public List<WeatherForecast> forecasts;
+        public CancellationTokenSource cts = new CancellationTokenSource();
+        protected override async Task OnInitializedAsync()
+        {
+            using var request = new HttpRequestMessage(HttpMethod.Get, "WeatherForecast");
+            request.SetBrowserResponseStreamingEnabled(true); // Enable response streaming
+            using var response = await Http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
+            using var stream = await response.Content.ReadAsStreamAsync();
+            await SetForcatsDataWithJsonTextReader(stream);
+            //await SetForcatsDataWithJsonTextReader(stream);
+        }
+
+        private async Task SetForcatsDataWithJsonTextReader(System.IO.Stream stream)
+        {
+            var serializer = new JsonSerializer();
+            forecasts = new List<WeatherForecast>();
+            using (var sr = new StreamReader(stream))
+            using (var jr = new JsonTextReader(sr))
+            {
+                while (await jr.ReadAsync(cts.Token))
+                {
+                    if (jr.TokenType != JsonToken.StartArray && jr.TokenType != JsonToken.EndArray)
+                    {
+                        forecasts.Add(serializer.Deserialize<WeatherForecast>(jr));
+                        StateHasChanged();
+                    }
+                }
+            }
+        }
+        private async Task SetForcatsDataWithStream(System.IO.Stream stream)
+        {
+            forecasts = new List<WeatherForecast>();
+            while (!cts.Token.IsCancellationRequested)
+            {
+
+                Memory<byte> buffer = new Memory<byte>(new byte[1000]);
+
+                var read = await stream.ReadAsync(buffer, cts.Token);
+
+                if (read == 0) // End of stream
+                    return;
+                var str = System.Text.Encoding.Default.GetString(buffer.ToArray());
+                Console.WriteLine(str);
+                try
+                {
+                    var wf = JsonConvert.DeserializeObject<WeatherForecast>(str);
+                    forecasts.Add(wf);
+                }
+                catch
+                {
+
+                }
+                StateHasChanged();
+            }
+        }
+    }
+}
---
+++ Webapi.Stream/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Webapi.Stream.Shared;
+
+namespace Webapi.Stream.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public async Task GetAsync(CancellationToken cancellationToken)
+        {
+            var rng = new Random();
+            var range = Enumerable.Range(1, 100).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray();
+            Response.Headers.Add("Content-Type", "text/event-stream");
+
+            for (int i = 0; i < range.Length; i++)
+            {
+                try
+                {
+                    cancellationToken.ThrowIfCancellationRequested();
+                }
+                catch
+                {
+                    throw;
+                }
+                await Task.Delay(rng.Next(100,1000));
+                string dataItem = $"{JsonConvert.SerializeObject(range[i])}";
+                if(i ==0)
+                {
+                    dataItem = "[" + dataItem;
+                    dataItem += ",";
+                }
+                else if(i == range.Length-1)
+                {
+                    dataItem = dataItem + "]";
+                }
+                else
+                {
+                    dataItem += ",";
+                }
+                byte[] dataItemBytes = ASCIIEncoding.ASCII.GetBytes(dataItem);
+                await Response.Body.WriteAsync(dataItemBytes, 0, dataItemBytes.Length);
+                await Response.Body.FlushAsync();
+            }
+        }
+        //[HttpGet]
+        //public HttpResponseMessage Get()
+        //{
+        //    var response = new HttpResponseMessage();
+        //    var content = new PushStreamContent(new Action<System.IO.Stream, HttpContent, TransportContext>(WriteContent), "application/json");
+
+        //    response.Headers.TransferEncodingChunked = true;
+        //    response.Content = content;
+
+        //    return response;
+        //}
+        //public static void WriteContent(System.IO.Stream stream, HttpContent content, TransportContext context)
+        //{
+        //    var serializer = JsonSerializer.CreateDefault();
+        //    var rng = new Random();
+        //    using (var sw = new StreamWriter(stream))
+        //    using (var jw = new JsonTextWriter(sw))
+        //    {
+        //        jw.WriteStartArray();
+        //        foreach (var id in Enumerable.Range(1, 10))
+        //        {
+        //            serializer.Serialize(jw, new WeatherForecast
+        //            {
+        //                Date = DateTime.Now.AddDays(id),
+        //                TemperatureC = rng.Next(-20, 55),
+        //                Summary = Summaries[rng.Next(Summaries.Length)]
+        //            });
+        //        }
+        //        jw.WriteEndArray();
+        //    }
+        //}
+        //[HttpGet]
+        //public async IAsyncEnumerable<WeatherForecast> GetAsync()
+        //{
+        //    var rng = new Random();
+        //    var range = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = rng.Next(-20, 55),
+        //        Summary = Summaries[rng.Next(Summaries.Length)]
+        //    }).ToArray();
+        //    Response
+        //    foreach(var r in range)
+        //    {
+        //        await Task.Delay(1000);
+        //        yield return r;
+        //    }
+        //}
+    }
+}
